e04579edcf4684dbed8ad65d105b6addf10e9f71
SOUMEN MONDAL <151416831+Soumen-JavaDev@users.noreply.github.com>
2025-04-13 10:02:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function shortcut(s1, s2) {
   // your code here
+	if(s1=="" || s2=="")
+		return "";
+	return s1.charAt(0)+s2.charAt(0);
 }
+
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
